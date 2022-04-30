aadf95140ca6dff3304e5f5dce4d1dabf1196bc2
Hugo Lima Romão <hugo8romao@gmail.com>
2022-04-30 11:49:30 -0400

    feat: create migrations

---
+++ migrations/Version20220430154659.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220430154659 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE client (id INT AUTO_INCREMENT NOT NULL, nome VARCHAR(50) NOT NULL, cpf INT NOT NULL, nascimento DATE NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE operator (id INT AUTO_INCREMENT NOT NULL, nome VARCHAR(20) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE phone (id INT AUTO_INCREMENT NOT NULL, client_id INT NOT NULL, operator_id INT DEFAULT NULL, ddd INT NOT NULL, numero INT NOT NULL, INDEX IDX_444F97DD19EB6921 (client_id), INDEX IDX_444F97DD584598A3 (operator_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL, available_at DATETIME NOT NULL, delivered_at DATETIME DEFAULT NULL, INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE phone ADD CONSTRAINT FK_444F97DD19EB6921 FOREIGN KEY (client_id) REFERENCES client (id)');
+        $this->addSql('ALTER TABLE phone ADD CONSTRAINT FK_444F97DD584598A3 FOREIGN KEY (operator_id) REFERENCES operator (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE phone DROP FOREIGN KEY FK_444F97DD19EB6921');
+        $this->addSql('ALTER TABLE phone DROP FOREIGN KEY FK_444F97DD584598A3');
+        $this->addSql('DROP TABLE client');
+        $this->addSql('DROP TABLE operator');
+        $this->addSql('DROP TABLE phone');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
