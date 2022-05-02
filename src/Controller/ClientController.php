ffb20696297a267e0b50314ed5b54c08b994919d
Hugo Lima Romão <hugo8romao@gmail.com>
2022-05-02 10:17:42 -0400

    create a instance of client successfully

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
+use App\Entity\Phone;
+use App\Repository\ClientRepository;
+use App\Repository\OperatorRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -27,22 +31,58 @@ class ClientController extends AbstractController
     /**
      * @Route("/client", methods={"POST"})
      */
-    public function create_client(Request $request): JsonResponse
+    public function create_client(Request $request, ClientRepository $client_repository, OperatorRepository $operator_repository): JsonResponse
     {
-        return $this->json(['route' => 'POST create_client', 'request' => $request->request]);
+        foreach (json_decode($request->getContent()) as $client)
+        {
+            $date = new \Datetime($client->nascimento);
+            $new_client = new Client();
+            $new_client->setNome($client->nome);
+            $new_client->setCpf($client->cpf);
+            $new_client->setNascimento($date);
+            var_dump($new_client->getNascimento());
+            exit;
+        //     foreach($client->telefones as $telefone)
+        //     {
+        //         $operadora = $operator_repository->find($telefone->operadora_id);
+        //         $new_telefone = new Phone();
+        //         $new_telefone->setDdd();
+        //         $new_telefone->setNumero();
+        //         $new_telefone->setOperator();
+        //         $new_telefone->setClient();
+        //         return $this->json($operadora);
+        //     }
+        }
+        // exit;
+        // $client_data = json_decode($request->getContent());
+        // return $this->json($client_data);
     }
     /**
      * @Route("/client/{id}", methods={"PUT"})
      */
-    public function update_client(int $id): JsonResponse
+    public function update_client(int $id, Request $request): JsonResponse
     {
-        return $this->json(['route' => 'PUT update_client with id: '.$id]);
+        // $params = json_decode($request->getContent(), true);
+        // foreach ($params as $obj) {
+        //     var_dump($obj);
+        // }
+        //$i = 0;
+        // for ($i; $i < count($params); $i++)
+        // {
+        //     var_dump($params[$i]);
+        // }
+        // exit;
+        return $this->json
+        ([
+            'route' => 'PUT update_client with id: '.$id,
+            'updated data' => json_decode($request->getContent(), true)
+        ]);
     }
     /**
      * @Route("/client/{id}", methods={"DELETE"})
      */
     public function delete_client(): JsonResponse
     {
-        return $this->json(['route' => 'DELETE delete_cliente']);
+        return $this->json(['route' => 'DELETE delete_client']);
     }
 }
