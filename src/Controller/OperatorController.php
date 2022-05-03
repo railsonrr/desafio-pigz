bbe9c07039302c06cedae59729ef845b049aaba1
Railson <rrrailson1@gmail.com>
2022-05-03 11:02:52 -0400

    feat: implement GET /operator route

--- src/Controller/OperatorController.php
+++ src/Controller/OperatorController.php
@@ -9,16 +9,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 class OperatorController extends AbstractController
 {
+
     /**
      * @Route("/operator", methods={"GET"})
      */
-    public function read_all_operators(Request $request): Response
+    public function read_all_operators(OperatorRepository $operator_repository, NormalizerInterface $serializer): Response
     {
-        return $this->json(['route' => 'GET read_all_operators']);
+        $operators_list = $operator_repository->findAll();
+        $operators_list_serialized = $serializer->normalize($operators_list, null, ['groups' => 'group1']);
+        return $this->json($operators_list_serialized);
     }
+
     /**
      * @Route("/operator/{id}", methods={"GET"})
      */
@@ -26,18 +31,20 @@ class OperatorController extends AbstractController
     {
         return $this->json(['route' => 'GET read_operator']);
     }
+
     /**
      * @Route("/operator", methods={"POST"})
      */
-    public function create_operator(Request $request, OperatorRepository $repository): JsonResponse
+    public function create_operator(Request $request, OperatorRepository $operator_repository): JsonResponse
     {
         foreach (json_decode($request->getContent()) as $operator) {
             $new_operator = new Operator();
             $new_operator->setNome($operator->nome);
-            $repository->add($new_operator);
+            $operator_repository->add($new_operator);
         }
         return $this->json("Saved new operator new operators");
     }
+
     /**
      * @Route("/operator/{id}", methods={"PUT"})
      */
@@ -59,6 +66,7 @@ class OperatorController extends AbstractController
             'updated data' => json_decode($request->getContent(), true)
         ]);
     }
+
     /**
      * @Route("/operator/{id}", methods={"DELETE"})
      */
