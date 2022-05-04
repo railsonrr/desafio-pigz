3bdffccee03f63e51fd227943c2aa85d32ae9724
Railson <rrrailson1@gmail.com>
2022-05-04 10:15:57 -0400

    improvement: only register client instead client with it phone(s)

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -39,28 +39,23 @@ class ClientController extends AbstractController
     /**
      * @Route("/client", methods={"POST"})
      */
-    public function create_client(Request $request, ClientRepository $client_repository, OperatorRepository $operator_repository, PhoneRepository $phone_repository): JsonResponse
+    public function create_client(Request $request, ClientRepository $client_repository): JsonResponse
     {
-        foreach (json_decode($request->getContent()) as $client)
+        $body = json_decode($request->getContent());
+        $clients_created_list = [];
+
+        foreach ($body as $client)
         {
             $date = new \Datetime($client->nascimento);
             $new_client = new Client();
             $new_client->setNome($client->nome);
             $new_client->setCpf($client->cpf);
             $new_client->setNascimento($date);
+
+            $clients_created_list[] = $new_client;
             $client_repository->add($new_client);
-            foreach($client->telefones as $telefone)
-            {
-                $operadora = $operator_repository->find($telefone->operadora_id);
-                $new_telefone = new Phone();
-                $new_telefone->setDdd($telefone->ddd);
-                $new_telefone->setNumero($telefone->numero);
-                $new_telefone->setOperator($operadora);
-                $new_telefone->setClient($new_client);
-                $phone_repository->add($new_telefone);
-            }
         }
-        return $this->json('Saved new clients');
+        return $this->json(['Saved new clients' => $clients_created_list]);
     }
 
     /**
@@ -68,27 +63,25 @@ class ClientController extends AbstractController
      */
     public function update_client(int $id, Request $request, ClientRepository $client_repository, NormalizerInterface $serializer): JsonResponse
     {
+        $client_body_request = json_decode($request->getContent());
         $clientFetched = $client_repository->find($id);
         // $data = $serializer->normalize($client, null, ['groups' => 'group1']);
 
-        foreach (json_decode($request->getContent()) as $clientRequest)
-        {
-            $date = new \Datetime($clientRequest->nascimento);
-            $clientFetched->setNome($clientRequest->nome);
-            $clientFetched->setCpf($clientRequest->cpf);
-            $clientFetched->setNascimento($date);
-            $client_repository->add($clientFetched);
-            // foreach($clientRequest->telefones as $telefone)
-            // {
-            //     $operadora = $operator_repository->find($telefone->operadora_id);
-            //     $new_telefone = new Phone();
-            //     $new_telefone->setDdd($telefone->ddd);
-            //     $new_telefone->setNumero($telefone->numero);
-            //     $new_telefone->setOperator($operadora);
-            //     $new_telefone->setClient($new_client);
-            //     $phone_repository->add($new_telefone);
-            // }
-        }
+        $date = new \Datetime($client_body_request->nascimento);
+        $clientFetched->setNome($client_body_request->nome);
+        $clientFetched->setCpf($client_body_request->cpf);
+        $clientFetched->setNascimento($date);
+        $client_repository->add($clientFetched);
+        // foreach($clientRequest->telefones as $telefone)
+        // {
+        //     $operadora = $operator_repository->find($telefone->operadora_id);
+        //     $new_telefone = new Phone();
+        //     $new_telefone->setDdd($telefone->ddd);
+        //     $new_telefone->setNumero($telefone->numero);
+        //     $new_telefone->setOperator($operadora);
+        //     $new_telefone->setClient($new_client);
+        //     $phone_repository->add($new_telefone);
+        // }
         return $this->json('Updated client');
     }
 
