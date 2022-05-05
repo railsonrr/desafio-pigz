630754b4b3673c52a53c71d4195762af95cfb9a0
Railson <rrrailson1@gmail.com>
2022-05-05 09:11:53 -0400

    feat: implement PUT /phone/{id}

--- src/Controller/PhoneController.php
+++ src/Controller/PhoneController.php
@@ -65,6 +65,32 @@ class PhoneController extends AbstractController
         $phones_serialized = $serializer->normalize($phone, null, ['groups' => 'group1']);
         return $this->json($phones_serialized);
     }
+    
+    /**
+     * @Route("/phone/{id}", methods={"PUT"})
+     */
+    public function update_phone(int $id, Request $request, PhoneRepository $phone_repository, OperatorRepository $operator_repository, ClientRepository $client_repository, NormalizerInterface $serializer): Response
+    {
+        $phone_body_request = json_decode($request->getContent());
+        $phone_fetched = $phone_repository->find($id);
+        $client_relationed = $client_repository->find($phone_body_request->client_id);
+        $operator_relationed = $operator_repository->find($phone_body_request->operator_id);
+
+        if (!$phone_fetched || !$client_relationed || !$operator_relationed)
+        {
+            return $this->json(null);
+        }
+
+        $phone_fetched->setDdd($phone_body_request->ddd);
+        $phone_fetched->setNumero($phone_body_request->numero);
+        $phone_fetched->setClient($client_relationed);
+        $phone_fetched->setOperator($operator_relationed);
+
+        $phone_repository->add($phone_fetched);
+
+        $phone_fetched_serialized = $serializer->normalize($phone_fetched, null, ['groups' => 'group1']);
+        return $this->json(['Update phone' => $phone_fetched_serialized]);
+    }
 
     /**
      * @Route("/phone/{id}", methods={"DELETE"})
